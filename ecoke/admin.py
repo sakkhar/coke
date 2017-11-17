ab2b81eb1dc6eda62db377263598282f275d14db
sahasakkhar <sakkhar@mail.com>
2017-11-17 16:31:21 +0600

    App that Coca-cola would know the brands of soft-drinks people drink in given locations Edit

--- ecoke/admin.py
+++ ecoke/admin.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
+from .models import Brand
+
 # Register your models here.
+class BrandAdmin(admin.ModelAdmin):
+    '''
+    Customizing the Brand Admin Dashboard
+    '''
+    list_display  = ('collector_name', 'respondent_name', 'respondent_city')
+    search_fields = ('collector_name', 'respondent_name', 'respondent_city')
+    list_filter   = ('date_of_collection',)
+    ordering      = ('-date_of_collection',)
+
+admin.site.register(Brand, BrandAdmin)
--- ecoke/models.py
+++ ecoke/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+
+    DRINKS =(
+        ('Cocacola', 'Coca-Cola'),
+        ('Sprite', 'Sprite'),
+        ('Fanta', 'Fanta'),
+        ('Merinda', 'Merinda'),
+        ('Pran Up', 'Pran Up'),
+        ('Seven Up', 'Seven Up'),
+        ('PW', 'Powerade'),
+        ('PZ', 'Powerade Zero'),
+        ('SO', 'Simply Orange'),
+        ('FS', 'Fresca'),
+        ('GV', 'Glaceau VitaminWater'),
+        ('GS', 'Glaceau SmartWater'),
+        ('DV', 'Del Valle'),
+        ('MY', 'Mello Yello'),
+        ('FZ', 'Fuze'),
+        ('FA', 'Fuze Tea'),
+        ('HT', 'Honest Tea'),
+        ('OD', 'Odwalla'),
+    )
+
+    collector_name       = models.CharField(max_length=255)
+    respondent_name      = models.CharField(max_length=255)
+    respondent_city      = models.CharField(max_length=255)
+    favourite_drink      = models.CharField(max_length=20, choices=DRINKS)
+    date_of_collection   = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s-%s' % (self.respondent_name, self.favourite_drink)
+
+    class Meta:
+        ordering = ['-date_of_collection', 'favourite_drink']
+
+
+
+
+
+
+
+
+
+
+
+
